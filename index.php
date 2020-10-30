3a233c6cd99bbe3bfe0daba28fd0ab14ce512b9b
63070132 <73680604+63070132@users.noreply.github.com>
2020-10-30 11:02:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,14 +7,15 @@
         <h1>Heading</h1>
         <p>Paragraph</p>
         <img src="coffee.jpg">
+        <img src="dowload.jpg">
         <br>
         <a href="-"http://www.it.kmitl.ac.th">Education Link</a>
 
-        <h2>The unordered list</h2>
-        <ul>
-            <li>Goolge</li>
-            <li>Yahoo</li>
-            <li>Amazom</li>
+        <h2>บทสนทนาของคู่รักคู่หนึ่ง
+ช : เราเลิกกันเถอะผมรู้ว่าคุณรักผมน้อยลง
+ญ : ฉันไม่เคยรักคุณน้อยลงนะ
+ช : น้อยสิ เดือนก่อนคุณรักผม 31 วัน เดือนนี้คุณรักผม 30 วัน
+ญ : พ่อง</h2>
         </ul>
 
     </body>
