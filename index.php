754c7c832a95f7a6429147a14fe2060fc3204eba
63070132 <73680604+63070132@users.noreply.github.com>
2020-10-30 10:59:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-<?php
-echo "<h2>PHP is Fun!</h2>
-echo "Hello world!<br>";
-?>
+  
+<html>
+    <head>
+        <title>Panupong</title>
+    </head>
+    <body>
+        <h1>Heading</h1>
+        <p>Paragraph</p>
+        <img src="coffee.jpg">
+        <br>
+        <a href="-"http://www.it.kmitl.ac.th">Education Link</a>
+
+        <h2>The unordered list</h2>
+        <ul>
+            <li>Goolge</li>
+            <li>Yahoo</li>
+            <li>Amazom</li>
+        </ul>
+
+    </body>
+</html>
